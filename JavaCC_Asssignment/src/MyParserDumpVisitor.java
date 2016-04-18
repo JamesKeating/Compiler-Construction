09d77290e3306514d805264d86e59129118748c2
James Keating <james.keating@fieldaware.com>
2016-04-18 11:46:16 +0100

    back up part 5

--- JavaCC_Asssignment/src/MyParserDumpVisitor.java
+++ JavaCC_Asssignment/src/MyParserDumpVisitor.java
@@ -1,189 +1,152 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MyParserDumpVisitor implements MyParserVisitor 
 {
-	  private int indent = 0;
+	  private int location = 0;
+	  private Map<SimpleNode, String> location_map = new HashMap<>();
 	  
-	  private String indentString(){
-		  StringBuffer sb = new StringBuffer();
-		  for (int i = 0; i < indent; ++i ){
-			  sb.append(' ');
-		  }
-		  return sb.toString();
+	  
+	  private String newLocation(){
+		  location++; 
+		  return "t" + Integer.toString(location);
 	  }
-	
+	  
 	  public Object visit(SimpleNode node, Object data){
-		  System.out.println(indentString() + node + "acceptor not implemented in subclass");
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
 	  }
 	  
 	  public Object visit(ASTStart node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
 	  }
 	  
-	  public Object visit(ASTBlock node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
-		  data = node.childrenAccept(this, data);
-		  --indent;
-		  return data;
-	  }
-	  
-	  public Object visit(ASTDeclset node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
-		  data = node.childrenAccept(this, data);
-		  --indent;
-		  return data;
-	  }
-	  
-	  public Object visit(ASTDeclset_Prime node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
-		  data = node.childrenAccept(this, data);
-		  --indent;
-		  return data;
-	  }
 	  
 	  public Object visit(ASTDecl node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
-	  }
-	  
-	  public Object visit(ASTVars node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
-		  data = node.childrenAccept(this, data);
-		  --indent;
-		  return data;
-	  }
-	  
-	  public Object visit(ASTVars_Prime node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
-		  data = node.childrenAccept(this, data);
-		  --indent;
-		  return data;
-	  }
-	  
-	  public Object visit(ASTStmtset node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
-		  data = node.childrenAccept(this, data);
-		  --indent;
-		  return data;
-	  }
-	  
-	  public Object visit(ASTStmtset_Prime node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
-		  data = node.childrenAccept(this, data);
-		  --indent;
-		  return data;
-	  }
-	  
-	  public Object visit(ASTStmt node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
-		  data = node.childrenAccept(this, data);
-		  --indent;
-		  return data;
-	  }
-	  
+	  }  
+	  	    
 	  public Object visit(ASTAssn node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
+		  ArrayList<String> code = new ArrayList<String>();
+		  code.add("Load");
+		  code.add(((SimpleNode) node.jjtGetChild(0)).value.toString());
+		  code.add("t" + Integer.toString(location));
+		  code.add("null");
+		  System.out.println(code);
 		  return data;
 	  }
 	  
 	  public Object visit(ASTArith node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
+		  if (node.children.length == 1)
+			  return data;
+		  
+		  String opCode = "Addition";
+		  ArrayList<String> code = new ArrayList<String>();
+		  SimpleNode child1 = (SimpleNode) node.jjtGetChild(0).jjtGetChild(0);
+		  SimpleNode child2= (SimpleNode) node.jjtGetChild(2).jjtGetChild(0).jjtGetChild(0);
+		  SimpleNode child3= (SimpleNode) node.jjtGetChild(1);
+		  
+		  if (child3.value.toString() == "-")
+			  opCode = "Subtraction";
+		  
+		  code.add(opCode);
+		  code.add(newLocation());
+		  code.add(location_map.get(child1));
+		  code.add(location_map.get(child2));
+		  System.out.println(code);
+		  
+		  location_map.put(child1, "t" +Integer.toString(location));	
 		  return data;
 	  }
 	  
 	  public Object visit(ASTArith_Prime node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
 	  }
 	  
 	  public Object visit(ASTTerm node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
+		  
+		  ArrayList<String> code = new ArrayList<String>();
+		  if (node.children.length == 1)
+			  return data;
+		  
+		  SimpleNode child1 = (SimpleNode) node.jjtGetChild(0);
+		  SimpleNode child2 = (SimpleNode) node.jjtGetChild(1).jjtGetChild(0);
+		  
+		  code.add("Multiply");  
+		  code.add(newLocation());
+		  code.add(location_map.get(child2));
+		  code.add(location_map.get(child1));
+		  System.out.println(code);
+		  
+		  location_map.put(child1, "t" +Integer.toString(location));	//??
+	  
 		  return data;
 	  }
 	  
 	  public Object visit(ASTTerm_Prime node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
 	  }
 	  
 	  public Object visit(ASTFactor node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
+
 		  data = node.childrenAccept(this, data);
-		  --indent;
+		  String opCode = "Load";
+		  ArrayList<String> code = new ArrayList<String>();
+		  SimpleNode child = (SimpleNode) node.jjtGetChild(0);
+		  if (child.value.toString().matches("[-+]?\\d*\\.?\\d+") )
+			  opCode = "Load_Literal";
+		  
+		  code.add(opCode);
+		  code.add(newLocation());
+		  code.add(child.value.toString());
+		  code.add("null");
+		  System.out.println(code);
+		  
+		  location_map.put(node, "t" +Integer.toString(location));
 		  return data;
 	  }
 	  
 	  public Object visit(ASTComp node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
 	  }
 	  
 	  public Object visit(ASTComp_Prime node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
 	  }
 	  
 	  public Object visit(ASTCond node, Object data){
-		  System.out.println(indentString() + node);
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
 	  }
 	  
 	  public Object visit(ASTID node, Object data){
-		  System.out.println(indentString() + node + "[" + node.value + "]" );
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
 		  return data;
 	  }
 	  
 	  public Object visit(ASTNum node, Object data){
-		  System.out.println(indentString() + node + "[" + node.value + "]" );
-		  ++indent;
 		  data = node.childrenAccept(this, data);
-		  --indent;
+		  return data;
+	  }
+	  
+	  public Object visit(ASTPlus node, Object data){
+		  data = node.childrenAccept(this, data);
+		  return data;
+	  }
+	  
+	  public Object visit(ASTMinus node, Object data){
+		  data = node.childrenAccept(this, data);
 		  return data;
 	  }
 }
