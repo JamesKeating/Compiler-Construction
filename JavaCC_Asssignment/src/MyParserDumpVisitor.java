0098227abcef302a39d31028ced67fd09c6c3912
James Keating <james.keating@fieldaware.com>
2016-04-19 21:22:28 +0100

    back up working

--- JavaCC_Asssignment/src/MyParserDumpVisitor.java
+++ JavaCC_Asssignment/src/MyParserDumpVisitor.java
@@ -48,46 +48,61 @@ public class MyParserDumpVisitor implements MyParserVisitor
 		  data = node.childrenAccept(this, data);
 		  ArrayList<String> code = new ArrayList<String>();
 		  SimpleNode child1 = (SimpleNode) node.jjtGetChild(0);
-		  String assigned_val = "t" + Integer.toString(location);
+		  //String assigned_val = "t" + Integer.toString(location);
+		  String assigned_val = location_map.get("assn");
+		  if  (assigned_val == null)
+			  assigned_val ="t" + Integer.toString(location);
+		  
 		  String assigned_type = "Int";
 		  
-		  if (node.jjtGetNumChildren() == 3){
-			  assigned_type = "Bool";
-			  assigned_val = memory.get(node.jjtGetChild(2));
+		  if (node.jjtGetNumChildren() == 4){
+			  if(node.jjtGetChild(3).jjtGetNumChildren() == 0){
+				  assigned_type = "Bool";
+				  assigned_val = memory.get(node.jjtGetChild(2));
+			  }
 		  }
 		  
-		  if (data_type.get(child1.value) != assigned_type)
-			  System.out.print("incorrect type");
-		
 		  
+		  if (data_type.get(child1.value) != assigned_type){
+			  System.out.print("incorrect type cannot assign bool to int or visa versa\nTerminating Program");
+			  	System.exit(1);
+		  }
+		  	
+		
 		  code.add("Load");
 		  code.add(child1.value.toString());
 		  code.add(assigned_val);
 		  code.add("null");
 		  
 		  System.out.println(code);
-		  memory.put(child1.value, memory.get("t" + Integer.toString(location)));
+		  
+		  if (node.jjtGetNumChildren() == 4 && assigned_type == "Int"){
+			  System.out.println("[Lable, "+"lb"+Integer.toString(lable) +", null, null]");
+			  if (((SimpleNode)node.jjtGetChild(3).jjtGetChild(2)).value == "false")
+			  	return data;
+		  }
+		  
+		  memory.put(child1.value, memory.get(assigned_val));
+		  location_map.remove("assn");
+		  //System.out.print(memory);
 		  return data;
 	  }
 	  
 	  
 	  public Object visit(ASTArith node, Object data){
 		  
-		  if (node.value == "false")
-			  return data;
 		  
 		  data = node.childrenAccept(this, data);
-		  if (node.children.length == 1)
+		  		  
+		  if (node.jjtGetNumChildren() == 1)
 			  return data;
-		  
-		  
-		  
+	  	
 		  String opCode = "Addition";
 		  ArrayList<String> code = new ArrayList<String>();
 		  SimpleNode child1 = (SimpleNode) node.jjtGetChild(0);
 		  SimpleNode child2= (SimpleNode) node.jjtGetChild(2).jjtGetChild(0);
 		  SimpleNode child3= (SimpleNode) node.jjtGetChild(1);
-		  
+		  //System.out.print(child1 + "---");
 		  
 		  if (child1.value == null)
 			  child1 = (SimpleNode) child1.jjtGetChild(0).jjtGetChild(0);
@@ -98,8 +113,17 @@ public class MyParserDumpVisitor implements MyParserVisitor
 		  if (child3.value.toString() == "-")
 			  opCode = "Subtraction";
 		  
+		  if (child1.value == null)
+			  child1 = (SimpleNode) child1.jjtGetChild(0);
+		  
+		  //System.out.print("sa"+child1+child2.value);
 		  if(data_type.get(child1.value) != "Int"|| data_type.get(child2.value)!= "Int" ){
 			  System.out.print("Type error cannot " + child3.toString() + " bool\nTerminating program");
+			  System.exit(1);
+		  }
+
+		  if((memory.get(child1.value) == null || memory.get(child2.value)!= null) && child1.toString() != "Num"){
+			  System.out.println("Cannot "+child3.toString()+" a null value. Make sure all variables are assigned non null value.\nTerminating program");
 			  System.exit(1);
 		  }
 		  
@@ -109,7 +133,22 @@ public class MyParserDumpVisitor implements MyParserVisitor
 		  code.add(location_map.get(child2.value));
 		  System.out.println(code);
 		  
-		  location_map.put(child1.value, "t" +Integer.toString(location));
+		  location_map.remove("assn");
+		  
+		  String val;
+		  if (code.get(0) == "Addition")
+			  val =String.valueOf(Integer.parseInt(memory.get(code.get(2))) +Integer.parseInt(memory.get(code.get(3))));   
+		  else
+			  val =String.valueOf(Integer.parseInt(memory.get(code.get(2))) - Integer.parseInt(memory.get(code.get(3))));
+		  
+		  memory.put("t" +Integer.toString(location), val);
+		
+		  ((SimpleNode)child1.jjtGetParent().jjtGetParent()).value = val;
+		  data_type.put(val, "Int");
+		  location_map.put(val, "t" +Integer.toString(location));
+		  //System.out.print(location_map);
+		  
+  
 		  return data;
 	  }	  
 	  
@@ -124,14 +163,29 @@ public class MyParserDumpVisitor implements MyParserVisitor
 		  SimpleNode child2 = (SimpleNode) node.jjtGetChild(1).jjtGetChild(0).jjtGetChild(0);
 		  node.value = newLocation();
 		  
-		  System.out.println(child1.value);
-		  System.out.println(child2.value);
 		  if(data_type.get(child1.value) != "Int"|| data_type.get(child2.value)!= "Int" ){
 			  System.out.print("Type error cannot multiple bool\nTerminating program");
 			  System.exit(1);
 		  }
+		  String v1,v2;
+		  if (child1.toString() == "Num")
+			  v1 = child1.value.toString();
 		  
-		  String result = Integer.toString(Integer.parseInt(memory.get(child2.value)) * Integer.parseInt(memory.get(child1.value)));
+		  else
+			  v1 = memory.get(child1.value);
+		  
+		  if (child2.toString() == "Num")
+			  v2 = child2.value.toString();
+		  
+		  else
+			  v2= memory.get(child2.value);
+		  
+		  if(v1 == null || v2 == null ){
+			  System.out.println("Cannot multiply a null value. Make sure all variables are assigned non null value.\nTerminating program");
+			  System.exit(1);
+		  }
+		  	
+		  String result = Integer.toString(Integer.parseInt(v1) * Integer.parseInt(v2));
 		  
 		  code.add("Multiply");  
 		  code.add(node.value.toString());
@@ -143,7 +197,8 @@ public class MyParserDumpVisitor implements MyParserVisitor
 		  location_map.put(node.value, node.value.toString());
 		  data_type.put(node.value, "Int");
 		  memory.put(node.value, result);
-		  
+		  location_map.remove("assn");
+		    
 		  return data;
 	  }
 	  
@@ -156,9 +211,16 @@ public class MyParserDumpVisitor implements MyParserVisitor
 		  ArrayList<String> code = new ArrayList<String>();
 		  SimpleNode child = (SimpleNode) node.jjtGetChild(0);
 		  
-		  if (location_map.containsKey(child.value))
+		//  System.out.print(child);
+		  if(child.toString() == "Arith")
 			  return data;
 		  
+    	  if (location_map.containsKey(child.value)){
+    		  location_map.put("assn", location_map.get(child.value.toString()));
+			  return data;
+    	  }
+    	  location_map.remove("assn");
+    	  
 		  if (loc  == null )
 			  loc = newLocation();
 		  
@@ -177,6 +239,7 @@ public class MyParserDumpVisitor implements MyParserVisitor
 		  System.out.println(code);
 		  
 		  location_map.put(child.value, loc);
+		  
 		  
 		  return data;
 	  }
@@ -234,9 +297,14 @@ public class MyParserDumpVisitor implements MyParserVisitor
 	  
 	  public Object visit(ASTCond node, Object data){
 		  
+		  if(node.jjtGetNumChildren() == 0){
+			  data = node.childrenAccept(this, data);
+			  return data;
+	  }
+		  
 		  ArrayList<String> code = new ArrayList<String>();
 		  SimpleNode child1 = (SimpleNode) node.jjtGetChild(0);
-		  SimpleNode child2= (SimpleNode) node.jjtGetChild(1);
+		  SimpleNode child2= (SimpleNode) node.jjtGetChild(2);
 		  SimpleNode parent = (SimpleNode) node.jjtGetParent().jjtGetChild(2);
 		  
 		  child1.value = memory.get(parent);
@@ -244,8 +312,20 @@ public class MyParserDumpVisitor implements MyParserVisitor
 			  child2.value = "false";
 		  else
 			  child2.value = "true";
-		  
+
 		  String opCode;
+		  
+		  SimpleNode loc1 = (SimpleNode) parent.jjtGetParent().jjtGetChild(1).jjtGetChild(0);
+		  SimpleNode loc2 = (SimpleNode) parent.jjtGetChild(1).jjtGetChild(0);
+
+		  
+		  if (loc1.value == null)
+			  loc1 = (SimpleNode) loc1.jjtGetChild(0).jjtGetChild(0);
+		  
+		  if (loc2.value == null)
+			  loc2 = (SimpleNode) loc2.jjtGetChild(0).jjtGetChild(0);
+		  
+		  
 		  switch (parent.value.toString()){
 		  case "=": opCode = "Jump_Equal";
 		  break;
@@ -258,10 +338,13 @@ public class MyParserDumpVisitor implements MyParserVisitor
 	  
 		  code.add(opCode);
 		  code.add(newLable());
-		  code.add("loc1");
-		  code.add("loc2");
+		  code.add(location_map.get(loc1.value));
+		  code.add(location_map.get(loc2.value));
 		  System.out.println(code);
-		  System.out.println("[Jump, " + newLable() +", null, null]");
+		  
+		  Node temp = node.children[0];
+		  node.children[0] = node.children[2];
+		  node.children[2] = temp;
 		  
 		  data = node.childrenAccept(this, data);  
 		  return data;
@@ -312,4 +395,22 @@ public class MyParserDumpVisitor implements MyParserVisitor
 		  return data;
 	  }
 	  
+	  public Object visit(ASTJump node, Object data){
+		  data = node.childrenAccept(this, data);
+		  String x = memory.get("assn");
+		  if (x == null)
+			  x ="t" + Integer.toString(location);
+		  
+	  	System.out.print("[Load, " 
+		 +((SimpleNode)node.jjtGetParent().jjtGetParent().jjtGetChild(0)).value.toString() 
+		 + ", " + x +" null]"
+		 +"\n[Jump, " + newLable() +", null, null]"
+		 +"\n[Lable, " +"lb"+Integer.toString(lable - 1)  + ", null, null]\n");
+		  
+		  if( ((SimpleNode)node.jjtGetParent().jjtGetChild(0)).value == "true" ){
+			  	memory.put(((SimpleNode)node.jjtGetParent().jjtGetParent().jjtGetChild(0)).value, memory.get(x));
+			  	location_map.remove("assn");
+		  }
+		  return data;
+	  }
 }
